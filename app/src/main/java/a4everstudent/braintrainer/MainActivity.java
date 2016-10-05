0d623d74917c53dbe06bfdbfe8cffabb97e755ec
4EverStudent <mail@4everstudent.com>
2016-10-05 16:24:13 +0100

    Initial commit

---
+++ app/src/main/java/a4everstudent/braintrainer/MainActivity.java
@@ -0,0 +1,24 @@
+package a4everstudent.braintrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// TODO: 05-10-2016 first screen - goButton
+// TODO: 05-10-2016 main screen :
+// TODO: 05-10-2016 timeLeft, question, correct/total questions, answers, feedback
+// TODO: 05-10-2016 countdowntimer connected to TimeLeft
+// TODO: 05-10-2016 array of questions/answers
+// TODO: 05-10-2016 variables correct answer, total questions
+// TODO: 05-10-2016 onClick on buttons, correct or wrong?
+// TODO: 05-10-2016 update function
+// TODO: 05-10-2016 last screen: show score, show button
+// TODO: 05-10-2016 playAgain function
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
